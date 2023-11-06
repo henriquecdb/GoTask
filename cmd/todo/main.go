09f795e8a40e9b651ab72bc553fcbc2dcdeb552a
henriquecdb <henriquecamposj@gmail.com>
2023-11-06 10:51:31 -0300

    Started main.go

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	todo "GoTask"
+	"flag"
 	"fmt"
+	"os"
+)
+
+const (
+	todoFile = ".todos.json"
 )
 
 func main() {
-	fmt.Println("Hello, world")
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as complete")
+
+	flag.Parse()
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
